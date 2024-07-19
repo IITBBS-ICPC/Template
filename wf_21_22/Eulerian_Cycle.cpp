afb9221f3c60d530431226fa3595aba32eeb5d10
yana449 <yanasinghal3107@gmail.com>
2024-07-19 18:07:46 +0530

    changing stacks to vectors due to memory performance

--- wf_21_22/Eulerian_Cycle.cpp
+++ wf_21_22/Eulerian_Cycle.cpp
@@ -1,38 +1,41 @@
 class EulerianCircuit{
     int n,e;
-    vector<stack<int>> adj;
+    vector<vector<int>> adj;
     vector<bool> visit;
     vector<int> edges;
     int get(int u){
-        while(visit[adj[u].top()])adj[u].pop();
-        return adj[u].top();
+        while(visit[adj[u].back()])adj[u].pop_back();
+        return adj[u].back();
     }
 public:
     EulerianCircuit(int _n):n(_n),e(0){
         adj.resize(n);
         for(int i=0;i<n;++i)
-            adj[i].push(0);
+            adj[i].pb(0);
         edges.resize(2);
         visit.resize(2);
     }
     void addEdge(int u,int v){
         ++e;
-        adj[u].push(e<<1);
+        adj[u].pb(e<<1);
         edges.push_back(v);
-        adj[v].push((e<<1)|1);
+        adj[v].pb((e<<1)|1);
         edges.push_back(u);
         visit.push_back(false);
         visit.push_back(false);
     }
-    vector<int> eulerCircuit(){
-        if(!e)return vector<int>();
-        stack<int> st;
+    int findFirst(){
         int u = 0;
         for(int i=0;i<n;++i){
             if(adj[i].size()==1)continue;
             u = i;
             if(!(adj[i].size()&1))break;
         }
+        return u;
+    }
+    vector<int> eulerCircuit(int u){
+        if(!e)return vector<int>();
+        stack<int> st;
         vector<int> circuit;
         st.push(u);
         while(!st.empty()){
@@ -47,5 +50,9 @@ public:
             }
         }
         return circuit;
+    }
+    vector<int> eulerCircuit(){
+        if(!e)return vector<int>();
+        return eulerCircuit(findFirst());
     }
 };
