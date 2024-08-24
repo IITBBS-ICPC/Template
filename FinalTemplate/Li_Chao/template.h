c4754d2f0c1039f31a96ce3348b23a31af36f3f8
AkshatGupta468 <74819556+AkshatGupta468@users.noreply.github.com>
2024-08-24 17:58:20 +0530

    Li chao template

--- FinalTemplate/Li_Chao/template.h
+++ FinalTemplate/Li_Chao/template.h
@@ -1,40 +1,42 @@
 // Important : Here range l,r denotes [l,r). With this a bug is avoided which is if we take [l,r] then when both l and r are negative the update [l,mid] can result in infinite 
 //             recursion. E.g: l = -7, r = -6. mid = (l+r)/2 = -6 so [l,mid] = [-7,-6], resulting in infinite recursion.
 
-const int N = 1000000;
-struct line{
-    ll m,c;
-    ll operator()(ll x) { return m*x + c; }
-}Lichao[5*N];
+typedef long long ll;
+struct line {
+    ll m, c;
+    ll operator()(ll x) { return m * x + c; }
+};
 
-void init() {
-    // Use default case m such that no input line contains that.
-    for(int i=0;i<5*N;++i) Lichao[i].m = -1;
-}
 
-void insert(int v,int l, int r,line cur) {
-    if(Lichao[v].m == -1) {  // No line in this region
-        Lichao[v] = cur;
-        return ;
+class Lichao {
+    vector<line> lc_tree;
+public:
+    Lichao(int N) {
+        lc_tree = vector<line>(N << 1, { 0, -(ll)(1e18) });
     }
-    if(l+1==r) {
-        if(Lichao[v](l) < cur(l)) Lichao[v] = cur;
-        return ;
+
+    void insert(int v, int l, int r, line cur) {
+        if (l + 1 == r) {
+            if (lc_tree[v](l) < cur(l)) lc_tree[v] = cur;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        int z = (mid - l) << 1;
+        if (lc_tree[v].m > cur.m) swap(lc_tree[v], cur);
+        if (cur(mid) > lc_tree[v](mid)) {
+            swap(lc_tree[v], cur);
+            insert(v + 1, l, mid, cur);
+        }
+        else
+            insert(v + z, mid, r, cur);
     }
-    int mid = (l+r)/2; 
-    if(Lichao[v].m > cur.m) swap(Lichao[v] , cur);
-    if(cur(mid) > Lichao[v](mid)) {
-        swap(Lichao[v] , cur);
-        insert(v<<1, l, mid, cur);
-    } else
-        insert((v<<1)|1, mid, r, cur);
-}
 
 
-ll query(int v, int l,int r, int pt) {       // Finding maximum value of function at x = pt
-    if(Lichao[v].m == -1) return -1e18;      // No line in this region
-    if(l+1==r) return Lichao[v](pt);
-    int mid = (l+r)/2;
-    if(pt <= mid) return max(Lichao[v](pt), query(v<<1, l, mid, pt));
-    else if(pt > mid) return max(Lichao[v](pt), query((v<<1)|1, mid, r, pt));
-}
+    ll query(int v, int l, int r, int pt) {       // Finding maximum value of function at x = pt
+        if (l + 1 == r) return lc_tree[v](pt);
+        int mid = (l + r) >> 1;
+        int z = (mid - l) << 1;
+        if (pt <= mid) return max(lc_tree[v](pt), query(v + 1, l, mid, pt));
+        else return max(lc_tree[v](pt), query(v + z, mid, r, pt));
+    }
+};
