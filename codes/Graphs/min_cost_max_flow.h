87aa603d018f3041983afff4098cb40cde5da7cb
Mohan Roy <rnroy1029@gmail.com>
2025-02-10 01:34:27 +0530

    added working mincost max flow code

--- codes/Graphs/min_cost_max_flow.h
+++ codes/Graphs/min_cost_max_flow.h
@@ -1,64 +1,92 @@
-namespace Flow {
-int fir[N],nxt[M],to[M],w[M],cst[M],ect = 1;
-inline void addedge(int u1,int v1,
-                        int w1,int c1) {
-    nxt[++ect] = fir[u1];fir[u1] = ect;
-    to[ect] = v1;w[ect] = w1;cst[ect] = c1;
+bool ckmin(int &a, int b) {
+  return b < a ? a = b, true : false;
 }
-inline void ins(int u1,int v1,int w1,int c1) { 
-    addedge(u1,v1,w1,c1);addedge(v1,u1,0,-c1);
+bool ckmin(ll &a, ll b) {
+  return b < a ? a = b, true : false;
 }
-int dis[N],h[N],vst[N],pw[N],pe[N];
-int tot;
-inline void Clr(int _n) {
-    tot = _n;ect = 1;
-    for(int i = 1;i <= tot;i++) 
-        fir[i] = h[i] = 0;
-}
-struct node {
-    int id,dis;
-    node(){}
-    node(const int _id,const int _dis):
-        id(_id),dis(_dis){}
-    bool operator < (const node &rhs) const { 
-        return dis > rhs.dis;
+
+struct MCMF {
+  using F = ll;
+  using C = ll; // flow type, cost type
+  struct Edge {
+    int to;
+    F flo, cap;
+    C cost;
+  };
+  int N;
+  vector<C> pi, dist;
+  vector<int> pre;
+  vector<Edge> eds;
+  vector<vector<int>> adj;
+  void init(int _N) {
+    N = _N;
+    pi.resize(N), dist.resize(N), pre.resize(N),
+      adj.resize(N);
+  }
+  void addEdge(int u, int v, F cap, C cost) {
+    assert(cap >= 0);
+    adj[u].pb(eds.size());
+    eds.pb({v, 0, cap, cost});
+    adj[v].pb(eds.size());
+    eds.pb({u, 0, 0, -cost});
+  } // use asserts, don't try smth dumb
+  bool path(int s, int t) { // find lowest cost path to
+                            // send flow through
+    const C inf = 1e18;
+    for(int i = 0; i < N; i++)
+      dist[i] = inf;
+    using T = pair<C, int>;
+    priority_queue<T, vector<T>, greater<T>> todo;
+    todo.push({dist[s] = 0, s});
+    while(todo.size()) { // Dijkstra
+      T x = todo.top();
+      todo.pop();
+      if(x.first > dist[x.second]) continue;
+      for(int e : adj[x.second]) {
+        const Edge &E = eds[e]; // all weights should be
+                                // non-negative
+        if(E.flo < E.cap
+           && ckmin(dist[E.to], x.first + E.cost
+                                  + pi[x.second]
+                                  - pi[E.to]))
+          pre[E.to] = e, todo.push({dist[E.to], E.to});
+      }
+    } // if costs are doubles, add some EPS so you
+    // don't traverse ~0-weight cycle repeatedly
+    return dist[t] != inf; // return flow
+  }
+  void setpi() { // Call this function before calc if
+                 // have -ve weights
+    for(int i = 0; i < N; i++) {
+      for(int e = 0; e < eds.size(); e++) {
+        const Edge &E = eds[e]; // Bellman-Ford
+        if(E.cap)
+          ckmin(pi[E.to], pi[eds[e ^ 1].to] + E.cost);
+      }
     }
+  }
+  pair<F, C> calc(int s, int t, int f) {
+    assert(s != t);
+    // setpi();
+    F totFlow = 0;
+    C totCost = 0;
+    while(path(s, t)
+          && f) { // p -> potentials for Dijkstra
+      for(int i = 0; i < N; i++)
+        pi[i] += dist[i]; // don't matter for
+                          // unreachable nodes
+      F df = f;
+      for(int x = t; x != s; x = eds[pre[x] ^ 1].to) {
+        const Edge &E = eds[pre[x]];
+        ckmin(df, E.cap - E.flo);
+      }
+      f -= df;
+      totFlow += df;
+      totCost += (pi[t] - pi[s]) * df;
+      for(int x = t; x != s; x = eds[pre[x] ^ 1].to)
+        eds[pre[x]].flo += df, eds[pre[x] ^ 1].flo
+                               -= df;
+    } // get max flow you can send along path
+    return {totFlow, totCost};
+  }
 };
-priority_queue<node> Q;
-bool Dijkstra(int S,int T) {
-    for(int i = 1;i <= tot;i++) 
-        dis[i] = 1e9,vst[i] = 0;
-    Q.emplace(S,dis[S] = 0);
-    while(!Q.empty()) {
-        int x = Q.top().id;Q.pop();
-        if(vst[x]) continue;
-        vst[x] = true;
-        for(int i = fir[x],y;y = to[i],i;i = nxt[i])
-            if(w[i] > 0 && dis[y] > 
-                dis[x] + cst[i] + h[x] - h[y]){
-                    Q.emplace(y,dis[y] = dis[x] + 
-                                cst[i] + h[x] - h[y]),
-                            pw[y] = x,pe[y] = i;
-            }
-    }
-    return dis[T] < 1e9;
-}
-int MCMF(int S,int T,int K) {
-    int res = 0,D = 0,flow = 0;
-    while(Dijkstra(S,T)) {
-        for(int i = 1;i <= tot;i++) h[i] += dis[i];
-        int f = 1e9;	
-        for(int i = T;i != S;i = pw[i]) 
-            f = min(f,w[pe[i]]);
-        if(h[T] != 0) f = min(f,D + K - flow);
-        if(!f) break;
-        for(int i = T;i != S;i = pw[i]) 
-            w[pe[i]] -= f,w[pe[i] ^ 1] += f;
-        flow += f;res += h[T] * f;
-        if(h[T] == 0) D = flow;
-    }
-    // printf("flow:%d\n",flow);
-    return res;
-}
-}
- 
